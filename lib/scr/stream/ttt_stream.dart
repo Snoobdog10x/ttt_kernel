6702541145055990bb582988c0897bb446882d58
Nguyễn Duy Thanh <thanhnd@alphalogy.net>
2024-11-02 21:05:51 +0700

    implement onValue and listen method

--- lib/scr/stream/ttt_stream.dart
+++ lib/scr/stream/ttt_stream.dart
@@ -1,8 +1,11 @@
 library ttt_stream;
 
+import 'dart:async';
 import 'dart:collection';
 
 import 'package:flutter/widgets.dart';
+
+import '../base/ttt_base.dart';
 
 part 'ttt_bool.dart';
 part 'ttt_list.dart';
@@ -12,8 +15,61 @@ part 'ttt_set.dart';
 part 'ttt_string.dart';
 part 'watcher.dart';
 
-class TttStream<T> extends ValueNotifier<T> {
+typedef StreamOnValue<T> = void Function(T value);
+
+class TttStreamSubscription<T> {
+  bool canceled = false;
+  late final TttStream<T> _stream;
+  final StreamOnValue<T> _onValue;
+
+  TttStreamSubscription(this._stream, this._onValue);
+
+  void _listener() {
+    _onValue(_stream.value);
+  }
+
+  void _listen() {
+    _stream.addListener(_listen);
+  }
+
+  void cancel() {
+    if (canceled) {
+      return;
+    }
+
+    canceled = true;
+    _stream.removeListener(_listener);
+  }
+}
+
+class TttStream<T> extends ValueNotifier<T> with LoggerMixin {
+  final Completer _completer = Completer();
+
   TttStream(super.value);
+
+  Future<void> onValue(StreamOnValue<T> onValue) async {
+    void listener() => onValue(value);
+    addListener(listener);
+    await _completer.future;
+    removeListener(listener);
+  }
+
+  TttStreamSubscription<T> listen(StreamOnValue<T> onValue) {
+    var subscription = TttStreamSubscription(this, onValue);
+    subscription._listen();
+    return subscription;
+  }
+
+  @override
+  void dispose() {
+    if (_completer.isCompleted) {
+      logError("Stream has been closed please init new one");
+      return;
+    }
+
+    _completer.complete();
+    super.dispose();
+  }
 
   @override
   T get value {
