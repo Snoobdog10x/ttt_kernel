56323df637b4bc0239fb1a3f845b1dc046c1ff24
Nguyễn Duy Thanh <thanhnd@alphalogy.net>
2024-12-04 19:06:17 +0700

    fix import

--- lib/scr/event_bus/event_bus_service.dart
+++ lib/scr/event_bus/event_bus_service.dart
@@ -5,7 +5,8 @@ import 'dart:async';
 import '../base/ttt_base.dart';
 
 part 'bloc.dart';
-part 'event_streaming_mixin.dart';t 'ttt_event.dart';
+part 'event_streaming_mixin.dart';
+part 'ttt_event.dart';
 
 class EventBusService extends TttService with EventStreamingMixin {
   static final EventBusService instance = EventBusService._();
